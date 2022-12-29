5c3cfa781ca211b6f60f8dd09808df548f9be4a3
Orlando Nunez <ocnunez@gmail.com>
2022-12-28 21:49:41 -0500

    add restart game button

--- app/src/main/java/com/orlandonunez/tictactoe/MainActivity.kt
+++ app/src/main/java/com/orlandonunez/tictactoe/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,9 +16,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.orlandonunez.tictactoe.Win.*
 import com.orlandonunez.tictactoe.ui.theme.TicTacToeTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -52,52 +49,48 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TicTacToeScreen() {
-    // true = players turn, false = AI's turn, null = no move
-    val playerTurn = remember { mutableStateOf(true) }
+    // true - player's turn, false - AI's turn
+    var playerTurn by remember { mutableStateOf(true) }
+
+    // true - player move, false - AI move, null - no move
     val moves = remember {
-        mutableStateListOf(
-            true,
-            null,
-            false,
+        mutableStateListOf<Boolean?>(
             null,
             null,
-            false,
-            true,
+            null,
+            null,
+            null,
+            null,
+            null,
             null,
             null
         )
     }
 
-    val win = remember { mutableStateOf<Win?>(null) }
+    var win by remember { mutableStateOf<Win?>(null) }
 
     val onTap: (Offset) -> Unit = {
-        if (playerTurn.value && win.value == null) {
+        if (playerTurn && win == null) {
             val x = (it.x / 333).toInt()
             val y = (it.y / 333).toInt()
-            val positionInMoves = y * 3 + x
-            if (moves[positionInMoves] == null) {
-                moves[positionInMoves] = true
-                playerTurn.value = false
-                win.value = checkGameOver(moves)
+            val posInMoves = y * 3 + x
+            if (moves[posInMoves] == null) {
+                moves[posInMoves] = true
+                playerTurn = false
+                win = checkEndGame(moves)
             }
         }
     }
 
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Tic Tac Toe", fontSize = 30.sp, modifier = Modifier.padding(16.dp))
 
-        Text(
-            text = "Tic Tac Toe",
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(16.dp)
-        )
-
-        Header(playerTurn.value)
+        Header(playerTurn)
 
         Board(moves, onTap)
 
         // AI moves
-        if (!playerTurn.value && win.value == null) {
+        if (!playerTurn && win == null) {
             CircularProgressIndicator(color = Color.Red, modifier = Modifier.padding(16.dp))
 
             val coroutineScope = rememberCoroutineScope()
@@ -108,32 +101,45 @@ fun TicTacToeScreen() {
                         val i = Random.nextInt(9)
                         if (moves[i] == null) {
                             moves[i] = false
-                            playerTurn.value = true
-                            win.value = checkGameOver(moves)
+                            playerTurn = true
+                            win = checkEndGame(moves)
                             break
                         }
                     }
                 }
             }
         }
-        if (win.value != null) {
-            when (win.value) {
-                Win.PLAYER -> {
+
+        if (win != null) {
+            when (win) {
+                PLAYER -> {
                     Text(text = "Player has won \uD83C\uDF89", fontSize = 25.sp)
                 }
-                Win.AI -> {
+
+                AI -> {
                     Text(text = "AI has won \uD83D\uDE24", fontSize = 25.sp)
                 }
-                Win.DRAW -> {
+
+                else -> {
                     Text(text = "It's a draw \uD83D\uDE33", fontSize = 25.sp)
                 }
-                else -> {}
+            }
+            // Restart Game
+            Button(onClick = {
+                playerTurn = true
+                win = null
+                for (i in 0..8) {
+                    moves[i] = null
+                }
+            }) {
+                Text(text = "Restart game")
             }
         }
     }
+
 }
 
-fun checkGameOver(m: List<Boolean?>): Win? {
+fun checkEndGame(m: List<Boolean?>): Win? {
     var win: Win? = null
     if (
         (m[0] == true && m[1] == true && m[2] == true) ||
@@ -145,7 +151,7 @@ fun checkGameOver(m: List<Boolean?>): Win? {
         (m[0] == true && m[4] == true && m[8] == true) ||
         (m[2] == true && m[4] == true && m[6] == true)
     )
-        win = Win.PLAYER
+        win = PLAYER
 
     if (
         (m[0] == false && m[1] == false && m[2] == false) ||
@@ -157,7 +163,7 @@ fun checkGameOver(m: List<Boolean?>): Win? {
         (m[0] == false && m[4] == false && m[8] == false) ||
         (m[2] == false && m[4] == false && m[6] == false)
     )
-        win = Win.AI
+        win = AI
 
     if (win == null) {
         var available = false
@@ -166,7 +172,7 @@ fun checkGameOver(m: List<Boolean?>): Win? {
                 available = true
         }
         if (!available)
-            win = Win.DRAW
+            win = DRAW
     }
     return win
 }
@@ -178,7 +184,7 @@ fun Header(playerTurn: Boolean) {
         horizontalArrangement = Arrangement.SpaceAround
     ) {
         val playerBoxColor = if (playerTurn) Color.Blue else Color.LightGray
-        val aiBoxColor = if (playerTurn) Color.LightGray else Color.Green
+        val aiBoxColor = if (playerTurn) Color.LightGray else Color.Red
 
         Box(
             modifier = Modifier
@@ -206,7 +212,6 @@ fun Header(playerTurn: Boolean) {
             )
         }
     }
-
 }
 
 @Composable
@@ -222,41 +227,35 @@ fun Board(moves: List<Boolean?>, onTap: (Offset) -> Unit) {
                 )
             }
     ) {
-        Column(
-            verticalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier.fillMaxSize()
-        ) {
+        Column(verticalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxSize(1f)) {
             Row(
                 modifier = Modifier
                     .height(2.dp)
-                    .fillMaxWidth()
+                    .fillMaxWidth(1f)
                     .background(Color.Black)
             ) {}
             Row(
                 modifier = Modifier
                     .height(2.dp)
-                    .fillMaxWidth()
+                    .fillMaxWidth(1f)
                     .background(Color.Black)
             ) {}
         }
-        Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier.fillMaxSize()
-        ) {
+        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxSize(1f)) {
             Column(
                 modifier = Modifier
                     .width(2.dp)
-                    .fillMaxHeight()
+                    .fillMaxHeight(1f)
                     .background(Color.Black)
             ) {}
             Column(
                 modifier = Modifier
                     .width(2.dp)
-                    .fillMaxHeight()
+                    .fillMaxHeight(1f)
                     .background(Color.Black)
             ) {}
         }
-        Column(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize(1f)) {
             for (i in 0..2) {
                 Row(modifier = Modifier.weight(1f)) {
                     for (j in 0..2) {
@@ -276,19 +275,19 @@ fun GetComposableFromMove(move: Boolean?) {
         true -> Image(
             painter = painterResource(id = R.drawable.ic_x),
             contentDescription = null,
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize(1f),
             colorFilter = ColorFilter.tint(Color.Blue)
         )
         false -> Image(
             painter = painterResource(id = R.drawable.ic_o),
             contentDescription = null,
-            modifier = Modifier.fillMaxSize(),
-            colorFilter = ColorFilter.tint(Color.Green)
+            modifier = Modifier.fillMaxSize(1f),
+            colorFilter = ColorFilter.tint(Color.Red)
         )
         null -> Image(
             painter = painterResource(id = R.drawable.ic_null),
             contentDescription = null,
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize(1f)
         )
     }
 }
